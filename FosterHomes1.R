04724c7daf8bffe41d7d7129783c3cbaba7a300b
Andy Barclay <abb@acm.org>
2019-01-04 14:52:34 -0500

    Fresh plc data

--- FosterHomes1.R
+++ FosterHomes1.R
@@ -1,8 +1,10 @@
 # abb 12/28/2018: Tally foster home boarding county/zip by removal county (not zip)
 
 stabb='GA'
-a1 <- read.table( '/fci/reports/ga/data/geocodes/recd20171208/afcars addresses 2017dec08.dsv.xz', header=T, sep='|', as.is=T, comment.char='', quote='"' )
-str( a1 ) # 68601 obs. of  16 variables
+Sys.umask('0133') # -rw-r--r--
+#a1 <- read.table( '/fci/reports/ga/data/geocodes/recd20171208/afcars addresses 2017dec08.dsv.xz', header=T, sep='|', as.is=T, comment.char='', quote='"' )
+a1 <- read.table( '/fci/reports/ga/data/geocodes/recd20190104a/afcars addresses, run 2019jan03.dsv.xz', header=T, sep='|', as.is=T, comment.char='', quote='"' )
+str( a1 ) # 75998 obs. of  16 variables
 # mostly zip+4, but zip5 less identifiable and probably sufficient.
 
 a1$remzip5 <- sub( '^([0-9]{5}).*$', '\\1', a1$REMOVAL_ZIP )
@@ -17,20 +19,22 @@ c1$CountyName <- paste( c1[,1], toupper(sub( ' +(parish|county|borough)$', '', c
 sort(table(cn1 <- paste( stabb, toupper(sub( ' +(parish|county|borough)$', '', a1$REMOVAL_COUNTY, ignore.case=T ))) ))
 table(!is.na(l11 <- match( cn1, c1$CountyName ))) # 1233 (1.8%) don't match
 table( cn1[is.na(l11)], useNA='always' ) # all blank or out of state
+stopifnot( cn1[is.na(l11)] %in% c('GA ', 'GA OUT OF STATE', 'GA -NONE-') )
 table( a1$remcofips <- c1$fips[l11], useNA='always' )
 
 sort(table(cn1 <- paste( stabb, toupper(sub( ' +(parish|county|borough)$', '', a1$PLACEMENT_COUNTY, ignore.case=T ))) ))
 table(!is.na(l11 <- match( cn1, c1$CountyName ))) # 6607 (9.6%) don't match
 table( cn1[is.na(l11)], useNA='always' ) # all none, na or out of state
+stopifnot( cn1[is.na(l11)] %in% c('GA ', 'GA OUT OF STATE', 'GA -NONE-') )
 table( a1$plccofips <- c1$fips[l11], useNA='always' )
 
 # convert zips to zcta centroids.
 tail(sort(table( a1$remzip5, useNA='always' )))
 table( nchar(as.character(a1$remzip5)), useNA='always' )
-#30060 30701 30349 30161 30032       
-#  662   682   697   747   906   937 
+#30060 30349 30701 30161       30032 
+#  705   750   784   798   829   969 
 #    0     5  <NA> 
-#  937 67664     0
+#  829 75169     0
 
 str(zc1 <- read.table( '/fci/reports/us/census/geonameszip_US.txt.xz', sep='\t', header=F, as.is=T, colClasses='character' ))
 colnames(zc1) <- unlist(strsplit('co zip zipname statename statefips countyname countyfips communityname communityfips lat lon accuracy',' '))
@@ -39,11 +43,10 @@ table(!is.na(l11 <- match( a1$remzip5, zc1[,2] ))) # 1072 (1.6%) don't match
 sort(table( as.character(a1$remzip5)[is.na(l11)], useNA='always' )) # 131 blank + erroneous
 tail(sort(table( a1$remzcta <- zc1[l11,2], useNA='always' )))
 summary(as.numeric(table( a1$remzcta )))
-#30060 30701 30349 30161 30032  <NA> 
-#  662   682   697   747   906  1072 
-#> summary(as.numeric(table( a1$remzcta )))
+#30060 30349 30701 30161 30032  <NA> 
+#  705   750   784   798   969  1048 
 #   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-#   1.00    2.00   17.00   70.49   84.00  906.00
+#   1.00    2.00   16.00   75.18   89.00  969.00
 tail(sort(table( a1$remzctalat <- zc1$lat[l11], useNA='always' )))
 tail(sort(table( a1$remzctalon <- zc1$lon[l11], useNA='always' )))
 
@@ -51,19 +54,22 @@ table(!is.na(l11 <- match( a1$plczip5, zc1[,2] ))) # 5819 (8.5%) don't match
 sort(table( as.character(a1$plczip5)[is.na(l11)], useNA='always' )) # 185 blank + erroneous
 tail(sort(table( a1$plczcta <- zc1[l11,2], useNA='always' )))
 summary(as.numeric(table( a1$plczcta )))
-#30127 30236 30349 31907 30058  <NA> 
-#  576   592   612   619   666  5819 
+#30701 31907 30349 30058 30236  <NA> 
+#  621   675   684   708   712  6014 
 #   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-#   1.00    1.00    3.00   39.56   31.00  666.00
+#   1.00    1.00    3.00   41.22   30.00  712.00
 tail(sort(table( a1$plczctalat <- zc1$lat[l11], useNA='always' )))
 tail(sort(table( a1$plczctalon <- zc1$lon[l11], useNA='always' )))
 
 # link a1 to AFCARS
+require(chron)
 system.time(load( '/fci/reports/ga/data/County_incare.RData' )) # 3s, d1, y, endofperiod, begofperiod
-prop.table(table(!is.na(l11 <- match( a1$AFCARS_ID, d1[,4] )))) # 98.9%, 728 unlinked
+pointintime <- as.Date(endofperiod) #as.Date('2018-09-30')
+d1 <- d1[order( as.numeric(d1[,21]), decreasing=T ),] # match most recent remdate
+prop.table(table(!is.na(l11 <- match( a1$AFCARS_ID, d1[,4] )))) # 97.8%, 1703 unlinked
 # todo: check unlinked.
-a1$plctp <- d1$plctp[l11]
-summary(a1$perdiem <- round( d1$MonthlyFCPayment[l11]/(365.25/12) ))
+a1$plctp <- d1$plctp[l11] # not using yet?
+summary(a1$perdiem <- round( d1$MonthlyFCPayment[l11]/(365.25/12) )) # using this
 
 # Accumulate indicators into frequency fields:
 # too big: dim(x2 <- table( 'RemovalZip'=a1$remzip5[i1], 'RemovalCounty'=a1$REMOVAL_COUNTY[i1], 'PlacementZip20171208'=a1$plczip5[i1], 'PlacementCounty'=a1$PLACEMENT_COUNTY[i1] ))
@@ -74,11 +80,14 @@ a1$p1 <- do.call( paste, c(a1[, f1], sep='|') )
 
 # limit to in foster care on lastday:
 summary(a1$lastday <- as.Date( a1$LAST_DAY, format='%d-%b-%Y' ))
+summary(a1$remdate <- as.Date( a1$REMOVAL_DATE, format='%d-%b-%Y' ))
 #        Min.      1st Qu.       Median         Mean      3rd Qu.         Max. 
-#"2001-06-06" "2011-11-17" "2014-10-16" "2014-06-04" "2017-05-17" "2017-12-08"
-table( i1 <- a1$lastday == max(a1$lastday) ) # 19.5%
+#"2001-06-06" "2012-03-14" "2015-05-26" "2014-12-31" "2018-03-05" "2019-01-03"
+#"1905-01-01" "2011-03-07" "2014-03-31" "2013-09-18" "2016-06-30" "2019-01-02"
+#table( i1 <- a1$lastday == max(a1$lastday) ) # 19.5%
+table( i1 <- a1$remdate <= pointintime & (is.na(a1$lastday) | a1$lastday >= pointintime) )
 #FALSE  TRUE 
-#55239 13362
+#62227 13771 2018-09-30
 
 # in care last day:
 dim(x1 <- data.frame(xtabs( ~ a1$p1[i1] ))) # 4628
@@ -86,29 +95,42 @@ sum(x1$Freq) # 13362
 x <- strsplit( as.character(x1[,1]), '|', fixed=T )
 head(x <- t(sapply( x,function(y) c(y[1:length(f1)]) )))
 colnames(x) <- f1
-str(x2 <- cbind( data.frame(x), 'In Foster Care 2017-12-08'=x1$Freq ))
-(units <- c( 'In Foster Care 2017-12-08'='Children' ))
+x2 <- cbind( data.frame(x), x1$Freq )
+units <- c( 'Children' )
+names(units)[length(units)] <- colnames(x2)[ncol(x2)] <- paste('In Foster Care', pointintime)
+str(x2)
 
 # with family:
 sort(table( a1$PLACEMENT_TYPE ))
+#write.csv( data.frame(sort(table( a1$PLACEMENT_TYPE ))), '', quote=F )
 nonfam <- !grepl( '^Relative|Parent|Non-Cust| Relative|Adopt', a1$PLACEMENT_TYPE, ignore.case=T )
 sort(table( a1$PLACEMENT_TYPE[nonfam] ))
-dim(x1 <- data.frame(xtabs( (!nonfam[i1]) ~ a1$p1[i1] ))) # 4628
-sum(x1$Freq) # 4324
-str(x2 <- cbind( x2, 'With Family 2017-12-08'=x1$Freq ))
-(units <- c( units, 'With Family 2017-12-08'='Children' ))
+paste(names(sort(table( a1$PLACEMENT_TYPE[nonfam] ), decreasing=T)), collapse=', ')
+paste(names(sort(table( a1$PLACEMENT_TYPE[!nonfam] ), decreasing=T)), collapse=', ')
+# DFCS Family Foster Home, CPA Family Foster Home, Child Care Institution, , Other Person, Runaway, Other Resource, YDC/RYDC, Hospital, Group Home, Emergency Shelter, Non-Relative Paid, CCI Family Foster Home, ICPC - Foster, ILP/Aftercare, Specialized Foster Home
+# Relative - Paid, Adoptive Home, Relative - Unpaid, Parent/Primary Caretaker, Relative Foster Home, Non-Custodial Parent, ICPC - Relative, ICPC - Adoptive
+dim(x1 <- data.frame(xtabs( (!nonfam[i1]) ~ a1$p1[i1] ))) # 4691
+sum(x1$Freq) # 4526
+str(x2 <- cbind( x2, x1$Freq ))
+(units <- c( units, 'Children' ))
+names(units)[length(units)] <- colnames(x2)[ncol(x2)] <- paste('With Family', pointintime)
+str(x2)
 
 # with non-family:
-dim(x1 <- data.frame(xtabs( nonfam[i1] ~ a1$p1[i1] ))) # 4628
-sum(x1$Freq) # 9038
-str(x2 <- cbind( x2, 'With Non-Family 2017-12-08'=x1$Freq ))
-(units <- c( units, 'With Non-Family 2017-12-08'='Children' ))
+dim(x1 <- data.frame(xtabs( nonfam[i1] ~ a1$p1[i1] ))) # 4691
+sum(x1$Freq) # 9245
+str(x2 <- cbind( x2, x1$Freq ))
+(units <- c( units, 'Children' ))
+names(units)[length(units)] <- colnames(x2)[ncol(x2)] <- paste('With Non-Family', pointintime)
+str(x2)
 
 # per-diems:
 dim(x1 <- data.frame(xtabs( pmax(0,a1$perdiem[i1],na.rm=T) ~ a1$p1[i1] ))) # 4628
 sum(x1$Freq) # 730348
-str(x2 <- cbind( x2, 'Total Per-Diem on 2017-12-08'=x1$Freq ))
-(units <- c( units, 'Total Per-Diem on 2017-12-08'='Dollars' ))
+str(x2 <- cbind( x2, x1$Freq ))
+(units <- c( units, 'Dollars' ))
+names(units)[length(units)] <- colnames(x2)[ncol(x2)] <- paste('Total Per-Diem on', pointintime)
+str(x2)
 
 # todo: non-family per-diems?
 
